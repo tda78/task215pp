db4fbed4e33e7ce2853765aeb8265fb580b5579d
tda78 <tda78@mail.ru>
2024-01-18 21:47:27 +0700

    ready

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,9 @@
 package koschei.models;
 
-public class Deth8 {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8{
+    public String toString(){return "";}
 }
